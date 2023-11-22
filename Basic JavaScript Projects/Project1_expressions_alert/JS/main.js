4b42e85ec6e9a73f09adf704f6ec2dd3c6733230
Ashleigh Godkin <ashgodkin@hotmail.com>
2023-11-22 16:03:31 +0000

    Add Project1 and Project 2 folders

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var HW = "Hello, World"; //Assign value to variable
+
+window.alert(HW); //Call alert
+
+var Sent1= ("This is the beginning of the string"); //Assign string to variable
+
+var Sent2= (" and this is the end of the string.") //Assign string to variable
+
+document.write(Sent1 + Sent2); //Concantenate 2 variable Sen1 + Sent2
+
+document.write(1+2); //Write expression
